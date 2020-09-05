b98885f51db59cd67b4fa8ca0fc8383cbc8adbec
Michael Fahim <michael_fahim@yahoo.com>
2020-09-05 16:24:32 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: %i[show create new edit]
+  resources :posts, only: %i[show update edit]
 end
